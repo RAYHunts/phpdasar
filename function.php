7e599cdaa0273e8c09823920e3143b18743811a2
RAYHunts <151200078@student.upnyk.ac.id>
2022-02-17 15:23:07 +0700

    finale 1

--- function.php
+++ function.php
@@ -1,4 +1,8 @@
-<?php   
+<?php 
+date_default_timezone_set('Asia/Jakarta');
+$date = date("Y-m-d");
+$time = date("H:i:s");  
+$today = date("l, d F Y");  
 $conn = mysqli_connect("localhost", "root", "", "madin");
 
 
@@ -15,12 +19,46 @@ function query( $query) {
 function add_user($new_user) {
     global $conn;
     
-    $nama = $new_user["name"];
-    $username = $new_user["username"];
-    $email = $new_user["email"];
-    $wali_kelas = $new_user["wali_kelas"];
-    $password = $new_user["password"];
-    $profile = $new_user["profile"];
+    $nama = htmlspecialchars($new_user["name"]);
+    $username = strtolower(stripslashes(htmlspecialchars($new_user["username"])));
+    $email = strtolower(htmlspecialchars($new_user["email"]));
+    $wali_kelas = htmlspecialchars($new_user["wali_kelas"]);
+    $password = mysqli_real_escape_string($conn, $new_user["password"]);
+    $password2 = mysqli_real_escape_string($conn, $new_user["password2"]);
+
+    $sameUser = mysqli_query($conn, "SELECT username FROM user WHERE username = '$username'");
+    $sameEmail = mysqli_query($conn, "SELECT email FROM user WHERE email = '$email'");
+
+    if( mysqli_fetch_assoc($sameUser)){
+        echo "<script>
+        alert('username telah terdaftar');
+        </script>";
+        return false;
+    }
+
+    if( mysqli_fetch_assoc($sameEmail)){
+        echo "<script>
+        alert('Email telah terdaftar');
+        </script>";
+        return false;
+    }
+
+    if ($password !== $password2){
+        echo "<script>
+        alert('konfirmasi password tidak sesuai');
+        </script>";
+        return false;
+    }
+
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+
+    //upload gambar 
+    $profile = upload();
+    if (!$profile) {
+        return false;
+    }
+    
     $level = "user";
 
     $query = "INSERT INTO user
@@ -32,11 +70,53 @@ function add_user($new_user) {
     return mysqli_affected_rows( $conn);
 }
 
+function upload(){
+    $namaFile = $_FILES['profile']['name'];
+    $ukuranFile = $_FILES['profile']['size'];
+    $error = $_FILES['profile']['error'];
+    $tmpName = $_FILES['profile']['tmp_name'];
+
+    if( $error === 4 ) {
+        echo "
+                <script>
+                alert('silahkan upload profile');
+                </script>";
+        return false;
+        
+    }
+
+    $ekstensiGambarValid = ['jpg', 'jpeg', 'png', 'gif'];
+    $ekstensiGambar = explode('.', $namaFile);
+    $ekstensiGambar = strtolower(end($ekstensiGambar));
+
+    if( !in_array($ekstensiGambar, $ekstensiGambarValid)) {
+        echo "<script>
+        alert('format gambar tidak didukung');
+        </script>";
+        return false;
+    }
+
+    //limit file size
+    if( $ukuranFile > 2000000){
+        echo "<script>
+        alert('ukuran gambar tidak boleh melebihi 2MB');
+        </script>";
+        return false;
+    }
+    $namaFileBaru = uniqid();
+    $namaFileBaru .= '.';
+    $namaFileBaru .= $ekstensiGambar;
+
+    move_uploaded_file($tmpName, 'img/' . $namaFileBaru);
+    return $namaFileBaru;
+    
+}
+
 
 function add_santri($new_user) {
     global $conn;
     
-    $nama = $new_user["nama"];
+    $nama = htmlspecialchars($new_user["nama"]);
 
     $query = "INSERT INTO data_santri
                 VALUES
@@ -46,5 +126,21 @@ function add_santri($new_user) {
 
     return mysqli_affected_rows( $conn);
 }
+
+function hapus_santri($id) {
+    global $conn;
+    mysqli_query( $conn, "DELETE FROM data_santri WHERE id = $id");
+
+    return mysqli_affected_rows($conn);
+}
+
+function hapus($id) {
+    global $conn;
+    mysqli_query( $conn, "DELETE FROM user WHERE id = $id");
+
+    return mysqli_affected_rows($conn);
+}
+
+
 
 ?>
